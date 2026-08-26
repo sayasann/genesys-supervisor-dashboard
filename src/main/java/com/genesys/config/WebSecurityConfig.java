df26eda22e0ed7ee2a38b962d32a9fdaa37e4554
Serkan Ayaşan <152634094+sayasann@users.noreply.github.com>
2026-08-26 14:19:06 +0300

    Spring Security is finished

--- src/main/java/com/genesys/config/WebSecurityConfig.java
+++ src/main/java/com/genesys/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.genesys.jwt.JwtAuthFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
